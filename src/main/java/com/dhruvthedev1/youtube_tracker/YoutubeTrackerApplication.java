928132301b3eaba1132c84bd6a40fe712345c97a
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-08-27 12:57:35 +0100

    Initial commit - springboot project setup

---
+++ src/main/java/com/dhruvthedev1/youtube_tracker/YoutubeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.youtube_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeTrackerApplication.class, args);
+	}
+
+}
